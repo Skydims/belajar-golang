5c1a79b4ce7ab9fbe4bda7f235a682028b32f800
DIMAS ADI SAPUTRA <110624244+Skydims@users.noreply.github.com>
2025-01-22 23:37:17 +0700

    Add files via upload

---
+++ handler/handler.go
@@ -0,0 +1,40 @@
+package handler
+
+import (
+	"log"
+	"net/http"
+	"path"
+	"text/template"
+)
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf(r.URL.Path)
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	tmpl, err := template.ParseFiles(path.Join("html", "index.html"))
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "lagi ada error bero ", http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "lagi ada nnuuu", http.StatusInternalServerError)
+		return
+	}
+
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("ok masbre"))
+}
+
+func DimasHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("ok wir"))
+}
